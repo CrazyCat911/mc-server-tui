054ccf621df1f8ffe56c869cf40c2b05bbf07e7d
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-12-24 10:07:16 +1000

    refactor

--- util/server_jar.py
+++ util/server_jar.py
@@ -2,68 +2,29 @@ import requests
 from packaging import version
 from req_session import session as s
 
-SUPPORTED_LOADERS = {"vanilla", "fabric", "quilt"}
+""" VANILLA """
+
+VANILLA_URL = "https://launchermeta.mojang.com"
 
 
-def get_supported_game_versions(loader: str) -> list[str]:
-    if loader not in SUPPORTED_LOADERS:
-        raise ValueError(f"Unsupported mod loader: {loader}")
-
-    supported_versions = []
-
-    if loader == "vanilla":
-        response = s.get(
-            "https://launchermeta.mojang.com/mc/game/version_manifest.json", timeout=5
-        )
-        vanilla_data: dict = response.json()
-        for vanilla_version in vanilla_data["versions"]:
-            if vanilla_version["type"] == "snapshot":
-                supported_versions.append(vanilla_version["id"])
-            elif vanilla_version["type"] == "release":
-                if version.parse(vanilla_version["id"]) >= version.parse(
-                    "1.2.5"
-                ):  # 1.2.5 is the version that added server jars
-                    supported_versions.append(vanilla_version["id"])
-
-    elif loader == "fabric":
-        response = s.get(
-            "https://meta.fabricmc.net/v2/versions/game", timeout=5
-        )  # see https://github.com/FabricMC/fabric-meta?tab=readme-ov-file#v2versionsgame
-        fabric_data: list[dict] = response.json()
-        supported_versions = [
-            fabric_version["version"] for fabric_version in fabric_data
-        ]
-
-    elif loader == "quilt":
-        response = s.get(
-            "https://meta.quiltmc.org/v3/versions/game", timeout=5
-        )  # see https://meta.quiltmc.org/#/v3/get_v3_versions_game
-        quilt_data: list[dict] = response.json()
-        supported_versions = [quilt_version["version"] for quilt_version in quilt_data]
+def get_supported_vanilla_game_versions() -> list[str]:
+    supported_versions: list[str] = []
+    response = s.get(f"{VANILLA_URL}/mc/game/version_manifest.json", timeout=5)
+    data: dict = response.json()
+    for obj in data["versions"]:
+        if obj["type"] == "snapshot":
+            supported_versions.append(obj["id"])
+        elif obj["type"] == "release":
+            if version.parse(obj["id"]) >= version.parse(
+                "1.2.5"
+            ):  # 1.2.5 is the version that added server jars
+                supported_versions.append(obj["id"])
 
     return supported_versions
 
 
-def get_supported_fabric_versions(game_version: str) -> list[str]:
-    response = s.get(
-        f"https://meta.fabricmc.net/v2/versions/loader/{game_version}", timeout=5
-    )  # See https://github.com/FabricMC/fabric-meta?tab=readme-ov-file#v2versionsloadergame_version
-    data: list[dict] = response.json()
-    return [obj["loader"]["version"] for obj in data]
-
-
-def get_supported_quilt_versions(game_version: str) -> list[str]:
-    response = s.get(
-        f"https://meta.quiltmc.org/v3/versions/loader/{game_version}", timeout=5
-    )  # See https://meta.quiltmc.org/#/v3/get_v3_versions_loader__game_version_
-    data = response.json()
-    return [obj["loader"]["version"] for obj in data]
-
-
 def get_vanilla_server_download_url(version: str, filepath: str) -> str:
-    response = s.get(
-        "https://launchermeta.mojang.com/mc/game/version_manifest.json", timeout=5
-    )
+    response = s.get(f"{VANILLA_URL}/mc/game/version_manifest.json", timeout=5)
     data = response.json()
 
     version_url: str | None = None
@@ -82,3 +43,51 @@ def get_vanilla_server_download_url(version: str, filepath: str) -> str:
     download_url = data["downloads"]["server"]["url"]
 
     return download_url
+
+
+""" FABRIC """
+
+FABRIC_URL = "https://meta.fabricmc.net"
+
+
+def get_supported_fabric_game_versions() -> list[str]:
+    response = s.get(
+        f"{FABRIC_URL}/v2/versions/game", timeout=5
+    )  # see https://github.com/FabricMC/fabric-meta?tab=readme-ov-file#v2versionsgame
+    data: list[dict] = response.json()
+    return [obj["version"] for obj in data]
+
+
+def get_supported_fabric_versions(game_version: str) -> list[str]:
+    response = s.get(
+        f"{FABRIC_URL}/v2/versions/loader/{game_version}", timeout=5
+    )  # See https://github.com/FabricMC/fabric-meta?tab=readme-ov-file#v2versionsloadergame_version
+    data: list[dict] = response.json()
+    return [obj["loader"]["version"] for obj in data]
+
+
+def get_fabric_installer_versions() -> list[str]:
+    response = s.get(f"{FABRIC_URL}/v2/versions/installer", timeout=5)
+    data: list[dict] = response.json()
+    return [obj["version"] for obj in data]
+
+
+""" QUILT """
+
+QUILT_URL = "https://meta.quiltmc.org"
+
+
+def get_supported_quilt_game_versions() -> list[str]:
+    response = s.get(
+        f"{QUILT_URL}/v3/versions/game", timeout=5
+    )  # see https://meta.quiltmc.org/#/v3/get_v3_versions_game
+    data: list[dict] = response.json()
+    return [obj["version"] for obj in data]
+
+
+def get_supported_quilt_versions(game_version: str) -> list[str]:
+    response = s.get(
+        f"{QUILT_URL}/v3/versions/loader/{game_version}", timeout=5
+    )  # See https://meta.quiltmc.org/#/v3/get_v3_versions_loader__game_version_
+    data = response.json()
+    return [obj["loader"]["version"] for obj in data]
