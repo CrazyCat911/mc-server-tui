b564c2e69c4c2b1db07e1e50338e35605e335aba
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-12-24 20:44:58 +1000

    Improve headers again

--- util/client.py
+++ util/client.py
@@ -2,6 +2,6 @@ import httpx
 
 client = httpx.AsyncClient(
     headers={
-        "User-Agent": "CrazyCat911's Minecraft Server TUI/0.0 https://github.com/CrazyCat911/mc-server-tui"
+        "User-Agent": "CrazyCat911's Minecraft Server TUI/0.0 (https://github.com/CrazyCat911/mc-server-tui)"
     }
 )
