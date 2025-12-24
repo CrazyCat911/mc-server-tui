d40875c7a2d23c38bd652acbb71703636e6bf209
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-12-24 12:06:01 +1000

    auto-focus

--- screens/setup_screen.py
+++ screens/setup_screen.py
@@ -1,5 +1,6 @@
 from textual.containers import VerticalScroll, Vertical
 from textual.app import ComposeResult
+from textual.events import Show
 from textual.widgets import Markdown, RadioSet, RadioButton
 import messages
 from util.server_exists import server_exists
@@ -8,6 +9,9 @@ from util.server_exists import server_exists
 class SetupScreen(VerticalScroll):
     CSS_PATH = "setup_screen.tcss"
     BORDER_TITLE = messages.SETUP_LONG
+
+    def on_show(self, event: Show) -> None:
+        self.query_one("#mod-loader-form", RadioSet).focus()
 
     def compose(self) -> ComposeResult:
         if False:  # server_exists(".")
@@ -18,7 +22,7 @@ class SetupScreen(VerticalScroll):
 
 class SetupForm(Vertical):
     def compose(self) -> ComposeResult:
-        with RadioSet():
+        with RadioSet(id="mod-loader-form"):
             yield RadioButton(messages.VANILLA_DESCRIPTION)
             yield RadioButton(messages.FABRIC_DESCRIPTION)
             yield RadioButton(messages.QUILT_DESCRIPTION)
