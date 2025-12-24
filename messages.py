7b0bbfcdd1ee34a2fefb9273c3c8f61f59b74785
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-12-24 13:11:42 +1000

    update forge colour

--- messages.py
+++ messages.py
@@ -14,8 +14,8 @@ QUILT_DESCRIPTION = Text.from_markup(
     "[purple b]Quilt[/] [bright_black i]A fork of [light_yellow3]Fabric[/][/]"
 )
 FORGE_DESCRIPTION = Text.from_markup(
-    "[grey27 b]Forge[/] [bright_black i]The OG. Very Slow and Clunky[/]"
+    "[grey78 b]Forge[/] [bright_black i]The OG. Very Slow and Clunky[/]"
 )
 NEOFORGED_DESCRIPTION = Text.from_markup(
-    "[dark_orange3 b]NeoForged[/] [bright_black i]The new [grey27]Forge[/]. Much better"
+    "[dark_orange3 b]NeoForged[/] [bright_black i]The new [grey78]Forge[/]. Much better"
 )
