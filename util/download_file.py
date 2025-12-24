dbfb34c41ae25c617f634def102ee620d43dcd78
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-12-24 12:43:38 +1000

    Improve Download file

--- util/download_file.py
+++ util/download_file.py
@@ -1,9 +1,17 @@
 from req_session import session as s
+from textual.widgets import ProgressBar
 
 
-def download_file(url: str, path: str) -> None:
+def download_file(
+    url: str, path: str, progress_bar: ProgressBar = ProgressBar()
+) -> None:
     response = s.get(url, stream=True)
+    chunk_size = 10 * 1024
 
-    with open(path, "wbx") as file:
-        for chunk in response.iter_content(chunk_size=10 * 1024):
+    content_length = response.headers.get("content-length")
+    progress_bar.total = float(content_length) if content_length is not None else None
+
+    with open(path, "xb") as file:
+        for chunk in response.iter_content(chunk_size=chunk_size):
             file.write(chunk)
+            progress_bar.advance(chunk_size)
